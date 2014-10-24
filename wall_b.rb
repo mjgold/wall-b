44dc8fb5dd8c94df826ec5f4eeaf4107a2ee17fa
Zee Spencer <zee@zeespencer.com>
2014-10-24 16:43:14 -0700

    Get rid of instance variables! WOO!

--- wall_b.rb
+++ wall_b.rb
@@ -10,7 +10,6 @@ end
 if ENV['RACK_ENV'] == "production"
   DataMapper.setup(:default, ENV['DATABASE_URL'])
 end
-
 # Here we're telling DataMapper to connect to the database specified by our
 # environment configuration.
 #
@@ -106,8 +105,8 @@ end
 get("/walls/new") do
   wall = Wall.new()
   # We're going to create a new wall, since `views/new_wall.erb` requires a
-  # `@wall` instance variable to auto-fill in the form.
-  body(erb(:new_wall, :locals => {:wall => wall}))
+  # `wall` local variable to auto-fill in the form.
+  body(erb(:new_wall, { :locals => {:wall => wall} }))
 end
 
 post("/walls") do
@@ -118,17 +117,17 @@ post("/walls") do
   wall_attributes["created_at"] = DateTime.now()
   # And add in the `created_at` attribute with the current time.
 
-  @wall = Wall.create(wall_attributes)
+  wall = Wall.create(wall_attributes)
   # Now we'll *create a wall* in our database from these attributes!
   # See:
   # * User Docs for Create: http://datamapper.org/docs/create_and_destroy.html
   # * Method docs for create: http://rdoc.info/github/datamapper/dm-core/DataMapper/Model#create-instance_method
 
-  if @wall.saved?()
+  if wall.saved?()
     redirect("/")
     # If we successfuly create the wall, let's send the user back home.
   else
-    body(erb(:new_wall))
+    body(erb(:new_wall), { :locals => { :wall => wall } })
     # If we *can't* create the wall; We'll redisplay the form so the user can
     # fix any errors.
   end
